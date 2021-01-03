849982608a506793836417cb27a9d1deaff1778d
kim <kim@DESKTOP-J8D1TR0>
2021-01-03 22:37:01 +0900

    ok

--- DogDrip/src/main/java/com/dogdrip/web/user/controller/UserController.java
+++ DogDrip/src/main/java/com/dogdrip/web/user/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.dogdrip.web.user.model.UserVO;
 import com.dogdrip.web.user.service.IUserService;
@@ -21,50 +22,65 @@ public class UserController {
 	private IUserService service;
 
 	
-	@GetMapping("signin")
+	@GetMapping("signIn")
 	public String signIn() {
 		System.out.println("/user/signin : GET");
 		
 		return "users/signin";
 	}
 	
-	@PostMapping("signin")
+	@PostMapping("signIn")
 	public String signIn(UserVO vo, HttpSession session,
-							Model model) {
+							Model model, RedirectAttributes ra) {
 		System.out.println("/user/signin : POST");
-		String result = null;
 		
 		UserVO dbData = service.selectOne(vo.getUserId());
 		session.setAttribute("signIn", dbData);
 		
 		if(dbData != null) {
 			if(vo.getPassword() == dbData.getPassword()) {
-				result = "signSuccess";
 			
 			}else {
-				result = "passwordFail";
+				ra.addFlashAttribute("msg", "passwordFail");
+				return "redirect:/board/login";
 			}
 		}else {
-			result = "idFail";
+			ra.addFlashAttribute("msg", "idFail");
+			return "redirect:/board/login";
 		}
-		model.addAttribute("msg", result);
-		
-		return "board/home";
+		ra.addFlashAttribute("msg", "signSuccess");
+		return "redirct:/board/home";
 	}
 	
-	@GetMapping("signup")
+	@GetMapping("signUp")
 	public String signUp() {
 		System.out.println("/user/signup : GET");
 		
 		return "user/signup";
 	}
 	
-	@PostMapping("signup")
+	@PostMapping("signUp")
 	public String signUp(UserVO vo) {
 		System.out.println("/user/signup : POST");
 		
 		service.signUpUser(vo);
 		return "user/login";
+	}
+	
+	@PostMapping("checkId")
+	public String checkId(@RequestBody String userId) {
+		System.out.println("/user/checkid : POST");
+		String result = null;
+		
+		if(service.checkId(userId) == 1) {
+			System.out.println("아이디 중복됨");
+			result = "idCheckFail";
+		}else {
+			System.out.println("아이디 사용가능");
+			result = "idCheckSuccess";
+		}
+		
+		return result;
 	}
 	
 }
